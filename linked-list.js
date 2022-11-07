1ee9279b212341958ddd2b2eb1d3f75d98554d96
Kat M <kmunson13@gmail.com>
2022-11-07 11:26:50 -0600

    Add functions

--- linked-list.js
+++ linked-list.js
@@ -12,5 +12,28 @@ const LinkedList = () => {
     const head = null
     let length = 0
 
-    const append  = (value) => {}
+    const append  = (value) => {
+        const newNode = Node(value)
+        length++
+        if (head === null) {return head=newNode}
+
+        let pointer = head
+        while (pointer.nextNode !== null) {
+            pointer = pointer.nextNode
+        }
+        pointer.nextNode = newNode
+    }
+
+    const size = () => {
+        if (head===null) {return null};
+        return length}
+
+    const headRead = () => {
+        if (head === null) {return null}
+        return head.value
+    }
+    
+    
+
+    return {append, size, headRead }
 }
